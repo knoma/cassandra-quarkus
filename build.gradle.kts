8034cf01dc1a2b53b8352a9af89403c7e7781033
knoma <knoma2010@gmail.com>
2023-12-12 08:47:30 -0500

    update version to 3.6.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,8 +31,8 @@ group = "com.knoma"
 version = "1.0.0-SNAPSHOT"
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
 }
 
 tasks.withType<Test> {
