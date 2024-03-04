cae7a2964f6022a2f0d1c0d3438819f81a0ce860
knoma <knoma2010@gmail.com>
2024-03-04 09:00:13 -0500

    update version to quarkus 3.8.1 and 8.5

--- src/main/java/com/knoma/pojo/Person.java
+++ src/main/java/com/knoma/pojo/Person.java
@@ -8,5 +8,9 @@ import com.datastax.oss.driver.api.mapper.annotations.PartitionKey;
 import java.util.UUID;
 
 @Entity(defaultKeyspace = "cass_drop")
-public record Person(@PartitionKey UUID id, String firstName, String lastName, String email) {
+public record Person(UUID id, String firstName, String lastName, String email) {
+    @PartitionKey
+    public UUID id() {
+        return id;
+    }
 }
